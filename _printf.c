8df792f376bdf3863bc6bb0023ae9acec1fe9b33
UDO MARY <111148417+ImeGits@users.noreply.github.com>
2023-10-17 18:36:33 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format.
- * by Imegits and Solution
+ * by Imegits and Solution(Alx Team Project)
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
