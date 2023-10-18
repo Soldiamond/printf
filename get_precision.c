f334a56ab2a284db8ea118d22ffaf5a4696297c9
Solution Abiola <abiolavctr@gmail.com>
2023-10-17 17:30:08 -0700

    Final Update get_precision.c

--- get_precision.c
+++ get_precision.c
@@ -4,7 +4,7 @@
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of arguments. (Alx Team Project)
+ * @list: list of arguments. (ImeGits and Soldiamond Alx Team Project)
  *
  * Return: Precision.
  */
