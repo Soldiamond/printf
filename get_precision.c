c38015954305a0285f8cfc3b05613e5d9cd64626
ImeGits <udomary963@gmail.com>
2023-10-17 17:47:06 -0700

    update

--- get_precision.c
+++ get_precision.c
@@ -1,13 +1,10 @@
 #include "main.h"
-<<<<<<< HEAD
-=======
 
->>>>>>> 69cb04f7c30632e4cf54e96e8058314ac12fb4ff
 /**
  * get_precision - Calculates the precision for printing
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
- * @list: list of arguments. (ImeGits and Soldiamond Alx Team Project)
+ * @list: list of arguments.
  *
  * Return: Precision.
  */
@@ -15,23 +12,14 @@ int get_precision(const char *format, int *i, va_list list)
 {
         int curr_i = *i + 1;
         int precision = -1;
-<<<<<<< HEAD
 
         if (format[curr_i] != '.')
                 return (precision);
 
         precision = 0;
+
         for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
         {
-=======
-        if (format[curr_i] != '.')
-                return (precision);
-        
-        precision = 0;
-        for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
-     
-                {
->>>>>>> 69cb04f7c30632e4cf54e96e8058314ac12fb4ff
                 if (is_digit(format[curr_i]))
                 {
                         precision *= 10;
@@ -45,14 +33,8 @@ int get_precision(const char *format, int *i, va_list list)
                 }
                 else
                         break;
-<<<<<<< HEAD
-	}
+        }
         *i = curr_i - 1;
-=======
-        
-                }
-                *i = curr_i - 1;
->>>>>>> 69cb04f7c30632e4cf54e96e8058314ac12fb4ff
 
         return (precision);
 }
